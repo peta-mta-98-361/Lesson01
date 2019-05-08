127de47b07540cddaf724c1200014793db420a5d
Michael Chittenden <michaeljchittenden@gmail.com>
2019-05-08 13:37:38 +0100

    FirstCommit

---
+++ Scenario1-1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quant;
+            while (true)
+            {
+                Console.WriteLine("How much of the product did you buy?");
+                try
+                {
+                    quant = Int32.Parse(Console.ReadLine());
+                    if (quant <= 0)
+                        throw new Exception("Quantity must be larger than 0 and smaller than 100.");
+                    Console.WriteLine();
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("You did not enter valid whole number.");
+                    Console.WriteLine();
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Your number is too big. Try again");
+                    Console.WriteLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine();
+                } 
+            }
+
+            int discount = discountCalc(quant);
+            Console.WriteLine("You get a discount of {0} percent.", discount);
+            
+            
+        }
+
+        public static int discountCalc(int quantity) {
+            int discountPercent;
+            if (quantity < 10)
+                discountPercent = 5;
+            else if (quantity < 50)
+                discountPercent = 10;
+            else if (quantity < 100)
+                discountPercent = 15;
+            else if (quantity >= 100)
+                discountPercent = 20;
+            else {
+                Console.WriteLine("Cannot compute discount.");
+                discountPercent = -1;
+            }
+
+            return discountPercent;
+        }
+    }
+}
---
+++ Scenario1-4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testNum;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("What number do you want to calculate?");
+                    testNum = Int32.Parse(Console.ReadLine());
+                    if (testNum <= 0)
+                        throw new Exception("The number must be positive.");
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("That is not a valid integer (decimals not supported). Try again.");
+                    Console.WriteLine();
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("That number is too large. Try again with a smaller integer.");
+                    Console.WriteLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine();
+                }
+            }
+           
+
+            int sigDig = sigDigRecursive(testNum);
+            if (testNum % 10 == 0)
+                sigDig--;
+            Console.WriteLine("There are {0} significant figures in the integer. (Note this is very basic and does not count 0s correctly)", sigDig);
+        }
+
+        public static int sigDigRecursive(int x) { 
+            if (x < 10)
+                return 1;
+            else
+                return 1 + sigDigRecursive(x / 10);
+        }
+    }
+}
